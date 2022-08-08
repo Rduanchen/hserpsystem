aefb3c3ca8146cf3a19e932b44c8feba3410c9f5
Rduanchen <chenyouduan@gmail.com>
2022-08-08 15:21:16 +0800

    third commit

---
+++ WEB_2/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    console.log("Jquery is ready to work"),
+    $('#leng_system').bind('click',)
+})
+function change_to_lendsystem(){
+    var req=new XMLHttprequest();
+    req.open("get","./Lend_web_lentsystem.html");
+    req.onload()=function(){
+        var a=document.getElementById("")
+    }   
+}
